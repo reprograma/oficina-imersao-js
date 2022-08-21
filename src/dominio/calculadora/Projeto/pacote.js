596c25f552a6a5c06234c1841adef908c675ce29
arushahg <arushahgomes@gmail.com>
2022-08-21 19:36:20 -0300

    ajuste nos arquivos para testes unitarios

--- src/dominio/calculadora/Projeto/pacote.js
+++ src/dominio/calculadora/Projeto/pacote.js
@@ -1,7 +1,7 @@
 const { MAX_HORAS_POR_PACOTE } = require('../constantes/constantes');
 
 const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_PACOTE)
-  .find(([key, value]) => value > totalDeHorasPorProjeto
+  .find(([key, value]) => value >= totalDeHorasPorProjeto
 )[0];
 
 exports.calcularPacote = calcularPacote;
