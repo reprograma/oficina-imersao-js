1ea9d7c4e2b184bdcb11868844d10b43bb13b655
Geisiane de Oliveira <geisianee.android@gmail.com>
2022-08-22 16:07:34 -0300

    Alteracao
    Alteracao

--- imersao-js-on22-oficina-3-main/src/dominio/calculadora/Hora/valorHora.js
+++ imersao-js-on22-oficina-3-main/src/dominio/calculadora/Hora/valorHora.js
@@ -6,7 +6,6 @@ const {
 const calcularValorPorHora = (rendaMensal) => {
   const horasTrabalhadasPorMes = (HORAS_POR_DIA * DIAS_UTEIS_NO_MES);
   const valorPorHora = rendaMensal / horasTrabalhadasPorMes;
-  /*A função Math.ciel arredonda para cima*/
   return Math.ceil(valorPorHora);
 };
 
