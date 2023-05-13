008e6198bdd47ae8292031f8e761620166c03f94
Roberta Amaro <roberta.amaro@ed3.com.br>
2023-05-13 15:49:25 -0300

    exemplos dos testes

---
+++ src/entregasDesafio/roberta-amaro/pacote.test.js
@@ -0,0 +1,30 @@
+const { calcularPacote } = require("../../dominio/calculadora/Projeto/pacote");
+
+describe("Calcular pacote de acordo com cada projeto", () => {
+  test("Retorna pacote básico caso o número total de horas seja até 50", () => {
+    const totalDeHorasPorProjeto = 49;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+    expect(result).toEqual("pacote_basico");
+  });
+  test("Retorna pacote básico caso so número seja exatamente 50", () => {
+    const totalDeHorasPorProjeto = 49;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+    expect(result).toEqual("pacote_basico");
+  });
+
+  test("Retorna pacote intermediário caso o número total de horas seja até 100", () => {
+    const totalDeHorasPorProjeto = 99;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+    expect(result).toEqual("pacote_intermediario");
+  });
+
+  test("Retorna pacote premium caso o número total de horas seja até 200", () => {
+    const totalDeHorasPorProjeto = 199;
+
+    const result = calcularPacote(totalDeHorasPorProjeto);
+    expect(result).toEqual("pacote_premium");
+  });
+});
