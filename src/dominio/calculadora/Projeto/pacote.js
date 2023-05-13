47c31bf717b2be318c926675560b009940ecd0f3
hinale <leticiaamarallara@hotmail.com>
2023-05-13 17:15:52 -0300

    revisão do desafio

--- src/dominio/calculadora/Projeto/pacote.js
+++ src/dominio/calculadora/Projeto/pacote.js
@@ -2,7 +2,8 @@ const { MAX_HORAS_POR_PACOTE } = require('../constantes/constantes');
 
 const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_PACOTE)
   .find(([key, value]) => value >= totalDeHorasPorProjeto
-  //Alterado sinal de >(maior que) para >=(maior que ou igual) para aceitar valores exatos no pacote como 50, 100, 200
+  //Alterado sinal de >(maior que) para >=(maior que ou igual) para aceitar valores exatos 
+  //no pacote como 50 para o pacote básico, 100 para o pacote intermediário, 200 para o pacote premium
 )[0];
 
 exports.calcularPacote = calcularPacote;
