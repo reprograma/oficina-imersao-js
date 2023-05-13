9823dfd6afd12ec94f46516a1073cbcbd6e25463
Marina Laurenio <laureniomarina@gmail.com>
2023-05-13 21:27:06 +0100

    desafio

--- src/dominio/calculadora/Projeto/pacote.js
+++ src/dominio/calculadora/Projeto/pacote.js
@@ -1,7 +1,19 @@
-const { MAX_HORAS_POR_PACOTE } = require('../constantes/constantes');
+const { MAX_HORAS_POR_PACOTE } = require("../constantes/constantes");
 
-const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_PACOTE)
-  .find(([key, value]) => value > totalDeHorasPorProjeto
-)[0];
+const calcularPacote = (totalDeHorasPorProjeto) => {
+  if(totalDeHorasPorProjeto === MAX_HORAS_POR_PACOTE.pacote_basico) {
+    return "pacote_basico"
+  }
+  if(totalDeHorasPorProjeto === MAX_HORAS_POR_PACOTE.pacote_intermediario) {
+    return "pacote_intermediario"
+  }
+  if(totalDeHorasPorProjeto === MAX_HORAS_POR_PACOTE.pacote_premium) {
+    return "pacote_premium"
+  }
+
+  return Object.entries(MAX_HORAS_POR_PACOTE).find(
+    ([key, value]) => value > totalDeHorasPorProjeto
+  )[0];
+};
 
 exports.calcularPacote = calcularPacote;
