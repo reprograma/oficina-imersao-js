bb9919eb0dcb2ab623fae20f2e67e9defd858e1f
Carla Marques <marques.carl97@gmail.com>
2023-05-14 11:44:09 -0300

    arquivos da oficina + solucao desafio

---
+++ src/entregasDesafio/carla-marques/horasPorProjeto.test.js
@@ -0,0 +1,8 @@
+const { calcularHorasDeProjeto } = require('../../dominio/calculadora/Projeto/horasPorProjeto');
+describe('Testes para o cálculo das horas totais para finalizar um projeto', () => {
+    test('Dado um array com as funcionalidades desejadas no projeto, retorna a soma das horas necessárias', () => {
+        const funcionalidades = ['setup','responsividade']
+        const result = calcularHorasDeProjeto(funcionalidades);
+        expect(result).toEqual(24);
+    });
+});
