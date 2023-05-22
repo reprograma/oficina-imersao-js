7890dee9613813069ed636220120e4507d92b1be
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-05-22 20:34:45 -0300

    feat: implement tests to valorProjeto

--- src/dominio/calculadora/Projeto/valorProjeto.js
+++ src/dominio/calculadora/Projeto/valorProjeto.js
@@ -1,19 +1,29 @@
-const { TAXAS_CONTRATUAIS_POR_PACOTE } = require('../constantes/constantes')
+const { TAXAS_CONTRATUAIS_POR_PACOTE } = require('../constantes/constantes');
 const { calcularHorasDeProjeto } = require('./horasPorProjeto');
 const { calcularPacote } = require('./pacote');
 
 const calcularValorBaseProjeto = (totalDeHorasPorProjeto, valorHora) => {
+  if (typeof totalDeHorasPorProjeto !== 'number' || totalDeHorasPorProjeto < 0) {
+    throw new Error('totalDeHorasPorProjeto must be greater than 0');
+  }
+
+  if (typeof valorHora !== 'number' || valorHora <= 0) {
+    throw new Error('valorHora must be greater than 0');
+  }
+
   return totalDeHorasPorProjeto * valorHora;
 };
 
 const calcularValorTotalProjeto = (funcionalidades, valorHora) => {
+  if (!Array.isArray(funcionalidades)) {
+    throw new Error('funcionalidades must be an array');
+  }
+
   const totalDeHorasPorProjeto = calcularHorasDeProjeto(funcionalidades);
-  
   const pacote = calcularPacote(totalDeHorasPorProjeto);
-
   const valorBase = calcularValorBaseProjeto(totalDeHorasPorProjeto, valorHora);
-
-  return Math.round(valorBase * TAXAS_CONTRATUAIS_POR_PACOTE[pacote]);
-}
+  const taxaContratual = TAXAS_CONTRATUAIS_POR_PACOTE[pacote] || 0;
+  return Math.round(valorBase * taxaContratual);
+};
 
 exports.calcularValorTotalProjeto = calcularValorTotalProjeto;
