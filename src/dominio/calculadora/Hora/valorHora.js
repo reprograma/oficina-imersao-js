9e7764f9f82e819da969be1c84404909a08bb79d
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-05-22 19:34:26 -0300

    feat: implement tests for valorHora

--- src/dominio/calculadora/Hora/valorHora.js
+++ src/dominio/calculadora/Hora/valorHora.js
@@ -1,13 +1,14 @@
-const {
-  HORAS_POR_DIA,
-  DIAS_UTEIS_NO_MES
-} = require('../constantes/constantes')
+const { HORAS_POR_DIA, DIAS_UTEIS_NO_MES } = require('../constantes/constantes');
 
 const calcularValorPorHora = (rendaMensal) => {
-  const horasTrabalhadasPorMes = (HORAS_POR_DIA * DIAS_UTEIS_NO_MES);
+  if (typeof rendaMensal !== 'number' || rendaMensal < 0) {
+    throw new Error('Invalid input!');
+  }
+  const horasTrabalhadasPorMes = HORAS_POR_DIA * DIAS_UTEIS_NO_MES;
   const valorPorHora = rendaMensal / horasTrabalhadasPorMes;
+  const valorPorHoraArredondado = Math.ceil(valorPorHora);
 
-  return Math.ceil(valorPorHora);
+  return valorPorHoraArredondado;
 };
 
 exports.calcularValorPorHora = calcularValorPorHora;
