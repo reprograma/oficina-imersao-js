97a1dac9e1d03c328fc1322a7cdae4f508fe20eb
Jeanne <jeannefernanda.m@gmail.com>
2023-08-05 17:54:07 -0400

    Testes adcionados

--- src/entregasDesafio/jeanne-mendes/valorProjeto.test.js
+++ src/entregasDesafio/jeanne-mendes/valorProjeto.test.js
@@ -7,7 +7,7 @@ describe("Valor total do projeto", ()=>{
     beforeEach(()=>{
         pacote.calcularPacote.mockReturnValue("pacote_basico")
     })
-    test("Retornar valor total de um projeto dado as funcionalidades e o valor da hora da pessoa dese", ()=>{
+    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa quando até 50h", ()=>{
         //setup 
         const funcionalidades = [
             "setup",
@@ -22,4 +22,75 @@ describe("Valor total do projeto", ()=>{
         //verificação
         expect(result).toEqual(3696)
     })
+
+    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa de 51h a 100h", ()=>{
+        //setup 
+        const funcionalidades = [
+            "setup",
+            "responsividade",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina"
+        ]
+        const valorHora = 70
+        //ação
+        const result = calcularValorTotalProjeto(funcionalidades,valorHora)
+        //verificação
+        expect(result).toEqual(4312)
+    })
+
+})
+
+describe("Valor total do projeto", ()=>{
+    beforeEach(()=>{
+        pacote.calcularPacote.mockReturnValue("pacote_intermediario")
+    })
+    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa de 51h a 100h", ()=>{
+        //setup 
+        const funcionalidades = [
+            "setup",
+            "responsividade",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina"
+        ]
+        const valorHora = 70
+        //ação
+        const result = calcularValorTotalProjeto(funcionalidades,valorHora)
+        //verificação
+        expect(result).toEqual(4390)
+    })
+
+})
+
+describe("Valor total do projeto", ()=>{
+    beforeEach(()=>{
+        pacote.calcularPacote.mockReturnValue("pacote_premium")
+    })
+    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa quando maior que 100h", ()=>{
+        //setup 
+        const funcionalidades = [
+            "setup",
+            "responsividade",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina",
+            "construcao_1_pagina"
+        ]
+        const valorHora = 70
+        //ação
+        const result = calcularValorTotalProjeto(funcionalidades,valorHora)
+        //verificação
+        expect(result).toEqual(9016)
+    })
+    
 })
