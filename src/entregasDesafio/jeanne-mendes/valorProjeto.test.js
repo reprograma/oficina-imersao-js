ef73c830fb25adedbdf0bb109c40726e1d1a55be
Jeanne <jeannefernanda.m@gmail.com>
2023-08-05 18:29:31 -0400

    Testes adcionados

--- src/entregasDesafio/jeanne-mendes/valorProjeto.test.js
+++ src/entregasDesafio/jeanne-mendes/valorProjeto.test.js
@@ -3,11 +3,11 @@ const pacote = require("../../dominio/calculadora/Projeto/pacote")
 
 jest.mock("../../dominio/calculadora/Projeto/pacote.js")
 
-describe("Valor total do projeto", ()=>{
+describe("Valor total do projeto básico: até 50h", ()=>{
     beforeEach(()=>{
         pacote.calcularPacote.mockReturnValue("pacote_basico")
     })
-    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa quando até 50h", ()=>{
+    test("Retorna o valor total para um projeto básico dado as funcionalidades pedidas e o valor da hora da desenvolvedora", ()=>{
         //setup 
         const funcionalidades = [
             "setup",
@@ -22,31 +22,13 @@ describe("Valor total do projeto", ()=>{
         //verificação
         expect(result).toEqual(3696)
     })
-
-    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa de 51h a 100h", ()=>{
-        //setup 
-        const funcionalidades = [
-            "setup",
-            "responsividade",
-            "construcao_1_pagina",
-            "construcao_1_pagina",
-            "construcao_1_pagina",
-            "construcao_1_pagina"
-        ]
-        const valorHora = 70
-        //ação
-        const result = calcularValorTotalProjeto(funcionalidades,valorHora)
-        //verificação
-        expect(result).toEqual(4312)
-    })
-
 })
 
-describe("Valor total do projeto", ()=>{
+describe("Valor total do projeto intermediário: de 51h até 100h", ()=>{
     beforeEach(()=>{
         pacote.calcularPacote.mockReturnValue("pacote_intermediario")
     })
-    test("Retornar o valor total de um projeto dado as funcionalidades e o valor/hora da pessoa de 51h a 100h", ()=>{
+    test("Retorna o valor total para um projeto premium dado as funcionalidades pedidas e o valor da hora da desenvolvedora", ()=>{
         //setup 
         const funcionalidades = [
             "setup",
@@ -63,9 +45,22 @@ describe("Valor total do projeto", ()=>{
         expect(result).toEqual(4390)
     })
 
+        test('Verifica se é do tipo número', () => {
+        //setup 
+        const funcionalidades = [
+            'construcao_1_pagina',
+            'construcao_1_pagina',
+        ];
+        const valorHora = 70;
+        //ação
+        const result = calcularValorTotalProjeto(funcionalidades, valorHora);
+        //verificação
+        expect(typeof result).toBe('number')
+    })
+
 })
 
-describe("Valor total do projeto", ()=>{
+describe("Valor total do projeto intermediário: de 101h até 200h", ()=>{
     beforeEach(()=>{
         pacote.calcularPacote.mockReturnValue("pacote_premium")
     })
@@ -92,5 +87,5 @@ describe("Valor total do projeto", ()=>{
         //verificação
         expect(result).toEqual(9016)
     })
-    
+ 
 })
